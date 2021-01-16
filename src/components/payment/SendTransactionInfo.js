7692bd816435bad9995d3addc6b810f02548b6e2
mosi <mosi@mosi.com>
2021-01-16 22:07:09 +0330

    payment

--- src/components/payment/SendTransactionInfo.js
+++ src/components/payment/SendTransactionInfo.js
@@ -1,68 +1,111 @@
-import React, { Component } from 'react';
-import { Button, message, Popconfirm } from 'antd';  
-import Axios from 'axios';
-import { config } from '../../Constant';
+import React, { Component } from "react";
+import { Button, Modal } from "antd";
+import Axios from "axios";
+import { config } from "../../Constant";
+import { Modal } from "bootstrap";
 
 var url = config.url.API_URL;
-const callback_url = "https://billlig.com/payment/verify/"
+const callback_url = "https://billlig.com/payment/verify/";
 
 class SendTransactionInfo extends Component {
+  state = {
+    token: "",
+    visible: false,
+  };
 
-    state = {
-        token: "",
-        visible: false
-    }
-    
-    handleOkinfo = () => {
-        this.setState({visible:false})
-    }
+  handleOkinfo = () => {
+    this.setState({ visible: false });
+  };
 
-    showInfo= () => {
-        this.setState({visible:true})
-    }
+  showInfo = () => {
+    this.setState({
+      visible: true,
+    });
+  };
 
-    sendapi = () => {
-        this.setState({visible:true})
-        const fee = this.props.fee
-        const token = localStorage.getItem('token');
-        const amount_w = this.props.amount;
-        const net_amount = ( 1 + (fee)/100 ) * amount_w * 10;
-        Axios.post(`${url}api/v1/payment/send/`,
-            { 
-             amount: net_amount,
-             callback_url: callback_url,
-             factorNumber: this.props.factorNumber,
+  cancleInfo = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  sendapi = () => {
+    this.setState({ visible: true });
+    const fee = this.props.fee;
+    const token = localStorage.getItem("token");
+    const amount_w = this.props.amount;
+    const net_amount = (1 + fee / 100) * amount_w * 10;
+    Axios.post(
+      `${url}api/v1/payment/send/`,
+      {
+        amount: net_amount,
+        callback_url: callback_url,
+        factorNumber: this.props.factorNumber,
+      },
+      { headers: { Authorization: `Token ${token}` } }
+    )
+      .then((res) => {
+        if (res.data.status === 1) {
+          return (
+            this.setState({ token: res.data.token }),
+            window.location.replace(
+              `https://ipg.vandar.io/v3/${res.data.token}`
+            )
+          );
+        }
+      })
+      .catch((error) =>{
+        notification["error"]({
+            message:error.reponse.data.error,
+            description:
+              "در مورد مبلغ مورد نظر خودتان با مسافر مذاکره کنید تا مبلغ پیشنهادی اصلاح شود",
+            style: {
+              fontFamily: "VazirD",
+              textAlign: "right",
+              float: "right",
+              width: "max-content",
             },
-            { headers: {"Authorization" : `Token ${token}`} })
-        .then( res => 
-            {if (res.data.status === 1 ) 
-            {  return (
-            this.setState({token: res.data.token}),
-            window.location.replace(`https://ipg.vandar.io/v3/${res.data.token}`))}
-        })
-        .catch(error => message.error(error.response.data ? error.response.data.errors : ""));
-    }
+            closeIcon: " ",
+            duration: 5,
+          });
+      }
+      );
+  };
 
-    render() {
-        return (
-            <div>
-            <Popconfirm
-            visible={this.state.visible}
-            onConfirm={this.sendapi}
-            overlayStyle={{ fontFamily: "VazirD" }}
-            cancelButtonProps={{ hidden: "true" }}
-            okText="متوجه شدم"
-            title={
-                <span>
-                طبق قوانین بیلیگ،‌ به میزان ۵ درصد به مبلغ تایید شده به عنوان کارمزد افزوده خواهد شد.
-                </span>
-            }
+  render() {
+    return (
+      <div>
+        <Modal
+          visible={this.state.visible}
+          onConfirm={this.sendapi}
+          style={{ fontFamily: "VazirD" }}
+          cancelText="انصراف"
+          okText="پرداخت"
+          width="70%"
+          title={
+            <p>
+              طبق قوانین بیلیگ،‌ به میزان {this.props.fee} درصد به مبلغ تایید
+              شده به عنوان کارمزد افزوده خواهد شد.
+              <br /> مبلغ پرداختی شما {this.props.amount} خواهد بود
+            </p>
+          }
+        >
+          <Button
+            onClick={this.showInfo.bind(this)}
+            style={{
+              fontSize: "12px",
+              border: "hidden",
+              backgroundColor: "green",
+              color: "white",
+              borderRadius: "10px",
+            }}
           >
-               <Button onClick={this.showInfo.bind(this)} style={{fontSize:"12px", border:"hidden", backgroundColor:"green", color:"white", borderRadius:"10px"}}>تایید و پرداخت</Button> 
-            </Popconfirm>
-            </div>
-        );
-    }
+            تایید و پرداخت
+          </Button>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default SendTransactionInfo;
